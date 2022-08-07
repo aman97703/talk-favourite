c1e860a5b8d7f8e969bb7fb0417fdbc20d217885
Aman Gupta <aman97703@gmail.com>
2022-08-07 16:34:58 +0530

    update bg

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://Talk-Favourite.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://talk-favourite.herokuapp.com/"; // "https://Talk-Favourite.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
